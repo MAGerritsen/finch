8c82d85912ba4c087d0e1d8c74ffa9d91c17ef20
ArraysZero <danibotland981@gmail.com>
2023-06-05 17:39:00 +0200

    puntentelling tweede berekening

--- finch/src/main/java/nl/han/ooad/classes/PuntentellingIngewikkeld.java
+++ finch/src/main/java/nl/han/ooad/classes/PuntentellingIngewikkeld.java
@@ -3,7 +3,6 @@ package nl.han.ooad.classes;
 public class PuntentellingIngewikkeld implements Puntentelling {
 	@Override
 	public int berekenPuntentelling(int juisteVragen, int tijd) {
-		// word niet geimplementeerd
-		return 0;
+		return (10 * juisteVragen) * (tijd / 60);
 	}
 }
